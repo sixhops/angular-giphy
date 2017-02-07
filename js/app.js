67c49e3142b5dff11462dcb9ec8889efdd7e123b
Steve Peters <sixhops@gmail.com>
2017-02-07 08:39:56 -0800

    Added controller to module and view

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
-angular.module('GiphyApp', [])
+var giphyApp = angular.module('GiphyApp', [])
 
 .run(function() {
   console.log('App has loaded!');
 });
+
+giphyApp.controller('GiphyCtrl', ['$scope', '$http', function($scope, $http) {
+	$scope.test_string = 'holy shit';
+}]);
