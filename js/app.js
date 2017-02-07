66340a24a41363c996620183b7abee7145538723
Steve Peters <sixhops@gmail.com>
2017-02-07 10:08:32 -0800

    Added searching, added results to view, moved ctrl higher in the DOM

--- js/app.js
+++ js/app.js
@@ -5,5 +5,26 @@ var giphyApp = angular.module('GiphyApp', [])
 });
 
 giphyApp.controller('GiphyCtrl', ['$scope', '$http', function($scope, $http) {
-	$scope.test_string = 'holy shit';
+	$scope.searchTerm = '';
+
+	$scope.search = function() {
+	  var req = {
+	    url: 'http://api.giphy.com/v1/gifs/search?',
+	    method: 'GET',
+	    params: {
+	      q: $scope.searchTerm,
+	      api_key: 'dc6zaTOxFJmzC',
+	      limit: 50
+	    }
+	  }
+
+	  $http(req).then(function success(res) {
+	    //do something with the response if successful
+	    console.log(res.data.data);
+	    $scope.results = res.data.data;
+	  }, function error(res) {
+	    //do something if the response has an error
+	    console.log(res);
+	  });
+	};
 }]);
